7d1b4cbd5bb472954d590da80f1e4ed57faced65
Richard Stupka <xstupk06@vutbr.cz>
2021-03-08 10:44:30 +0100

    cvičeni4[A

--- labs/04-segment/README.md
+++ labs/04-segment/README.md
@@ -1,6 +1,6 @@
 # 04-Segment
 ## 1. Preparation tasks (done before the lab at home). Submit
-![1](images/NexysLed.PNG)
+![1](images/schema.PNG)
 ### Figure or table with connection of 7-segment displays on Nexys A7 board
 ### Decoder truth table for common anode 7-segment display.
 | Hex | Inputs | A | B | C | D | E | F | G |
@@ -153,3 +153,35 @@ end Behavioral;
 | d | 1101 |0  |1  |1  | 0 |
 | E | 1110 |0  |1  |0  | 0 |
 | F | 1111 |0  |1  |1  | 0 |
+## listing of VHDL code for LEDs(7:4) with syntax highlighting
+architecture Behavioral of top is
+
+begin
+   --------------------------------------------------------------------
+    -- Instance (copy) of hex_7seg entity
+        hex2seg : entity work.hex_7seg
+            port map(
+                hex_i    => SW,
+                seg_o(6) => CA,
+                seg_o(5) => CB,
+                seg_o(4) => CC,
+                seg_o(3) => CD,
+                seg_o(2) => CE,
+                seg_o(1) => CF,                
+                seg_o(0) => CG
+            );
+    
+    -- Connect one common anode to 3.3V
+    AN <= b"1111_0111";
+
+            -- Display input value
+   LED(3 downto 0) <= SW;
+   LED(4)  <= '1' when (SW = "0000") else '0';
+   LED(5)  <= '1' when (SW > "1001") else '0';
+   LED(6)  <= '1' when (SW = "0001" or SW = "0011" or SW = "0101" or SW = "0111" or SW = "1001" or SW = "1011" or SW = "1101" or SW = "1111") else '0';
+   LED(7)  <= '1' when (SW = "0001" or SW = "0010" or SW = "0100" or SW = "1000") else '0';
+
+end Behavioral;
+```
+## Screenshot with simulated time waveforms; always display all inputs and outputs.
+![1](images/simulace2.PNG)
--- labs/04-segment/display/display.srcs/sim_1/new/tb_top.vhd
+++ labs/04-segment/display/display.srcs/sim_1/new/tb_top.vhd
@@ -1,69 +1,92 @@
-
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 05.03.2021 19:49:24
+-- Design Name: 
+-- Module Name: tb_top - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
 
 
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
 
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
 
 entity tb_top is
 --  Port ( );
 end tb_top;
 
 architecture Behavioral of tb_top is
- -- Local signals
-     signal s_SW : STD_LOGIC_VECTOR (4-1 downto 0);
-     signal s_CA :  STD_LOGIC;
-     signal s_CB :  STD_LOGIC;
-     signal s_CC :  STD_LOGIC;
-     signal s_CD :  STD_LOGIC;
-     signal s_CE :  STD_LOGIC;
-     signal s_CF :  STD_LOGIC;
-     signal s_CG :  STD_LOGIC;
-     
-     signal s_LED : std_logic_vector (8-1 downto 0);
-     signal s_AN  : std_logic_vector (8-1 downto 0);
+
+    signal s_SW : STD_LOGIC_VECTOR (4 - 1 downto 0); -- Input binary data
+    signal s_CA : STD_LOGIC;  
+    signal s_CB : STD_LOGIC;  
+    signal s_CC : STD_LOGIC;  
+    signal s_CD : STD_LOGIC;  
+    signal s_CE : STD_LOGIC;  
+    signal s_CF : STD_LOGIC;  
+    signal s_CG : STD_LOGIC; 
+        
+    signal s_LED : STD_LOGIC_VECTOR (8 - 1 downto 0); 
+    signal s_AN  : STD_LOGIC_VECTOR (8 - 1 downto 0); 
+
 begin
- -- Connecting testbench signals with comparator_2bit entity (Unit Under Test)
-    uut_comparator_2bit : entity work.top
+
+    uut_top : entity work.top
         port map(
-           SW =>        s_SW  , 
-           CA =>        s_CA  , 
-           CB =>        s_CB  ,
-           CC =>        s_CC  , 
-           CD =>        s_CD  , 
-           CE =>        s_CE  , 
-           CF =>        s_CF  , 
-           CG =>        s_CG  , 
-                         
-           LED  =>       s_LED,
-           AN   =>       s_AN 
-                         
-           
+            SW           => s_SW,
+            CA           => s_CA,
+            CB           => s_CB,
+            CC           => s_CC,
+            CD           => s_CD,
+            CE           => s_CE,
+            CF           => s_CF,
+            CG           => s_CG,
+            LED          => s_LED,
+            AN           => s_AN
         );
-    p_stimulus : process
+
+p_stimulus : process
     begin
-        -- Report a note at the beginning of stimulus process
+        -- Report a note at the begining of stimulus process
         report "Stimulus process started" severity note;
 
+        s_SW <= "0000"; wait for 100 ns;
+        s_SW <= "0001"; wait for 100 ns;
+        s_SW <= "0010"; wait for 100 ns;
+        s_SW <= "0011"; wait for 100 ns;
+        s_SW <= "0100"; wait for 100 ns;
+        s_SW <= "0101"; wait for 100 ns;
+        s_SW <= "0110"; wait for 100 ns;
+        s_SW <= "0111"; wait for 100 ns;
+        s_SW <= "1000"; wait for 100 ns;
+        s_SW <= "1001"; wait for 100 ns;
+        s_SW <= "1010"; wait for 100 ns;
+        s_SW <= "1011"; wait for 100 ns;
+        s_SW <= "1100"; wait for 100 ns;
+        s_SW <= "1101"; wait for 100 ns;
+        s_SW <= "1110"; wait for 100 ns;
+        s_SW <= "1111"; wait for 100 ns;
 
-        s_SW <= "0000"; wait for 100 ns;   --0
-        s_SW <= "0001"; wait for 100 ns;   --1
-        s_SW <= "0010"; wait for 100 ns;   --2
-        s_SW <= "0011"; wait for 100 ns;   --3
-        s_SW <= "0100"; wait for 100 ns;   --4
-        s_SW <= "0101"; wait for 100 ns;   --5
-        s_SW <= "0110"; wait for 100 ns;   --6
-        s_SW <= "0111"; wait for 100 ns;   --7
-        s_SW <= "1000"; wait for 100 ns;   --8
-        s_SW <= "1001"; wait for 100 ns;   --9
-        s_SW <= "1010"; wait for 100 ns;   --a
-        s_SW <= "1011"; wait for 100 ns;   --b
-        s_SW <= "1100"; wait for 100 ns;   --c
-        s_SW <= "1101"; wait for 100 ns;   --d
-        s_SW <= "1110"; wait for 100 ns;   --e
-        s_SW <= "1111"; wait for 100 ns;   --f
-       
         -- Report a note at the end of stimulus process
         report "Stimulus process finished" severity note;
         wait;
